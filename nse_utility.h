e7898d5ddcefc5b6792ecb5de57dcd3cdc45f407
batrick <batrick@e0a8ed71-7df4-0310-8962-fdc924857419>
2012-01-26 04:14:14 +0000

    Namespace utility functions. Use Lua 5.2 lua_absindex.
    git-svn-id: https://svn.nmap.org/nmap-exp/patrick/nmap@27900 e0a8ed71-7df4-0310-8962-fdc924857419

--- nse_utility.h
+++ nse_utility.h
@@ -1,19 +1,117 @@
 #ifndef NMAP_NSE_UTILITY_H
 #define NMAP_NSE_UTILITY_H
 
-size_t table_length (lua_State *, int);
-void setsfield (lua_State *, int, const char *, const char *);
-void setnfield (lua_State *, int, const char *, lua_Number);
-void setbfield (lua_State *, int, const char *, int);
-void weak_table (lua_State *, int, int, const char *);
+#include <stdint.h>
 
-int success (lua_State *);
-int safe_error (lua_State *, const char *, ...);
+/* void nseU_tablen (lua_State *L, int idx)                [-0, +0, -]
+ *
+ * Calculates the number of entries in the table by iterating over
+ * each key/value pair.
+ */
+size_t nseU_tablen (lua_State *L, int idx);
 
-void check_target (lua_State *, int, const char **, const char **);
-unsigned short check_port (lua_State *, int, const char **);
+/* void nseU_setsfield (lua_State *L, int idx,             [-0, +0, e]
+ *                      const char *field, const char *value)
+ *
+ * Sets the field for table at index idx to string value.
+ *  (t[field] = value).
+ */
+void nseU_setsfield (lua_State *L, int idx, const char *field, const char *value);
 
-Target *get_target (lua_State *, int);
-Port *get_port (lua_State *, Target *, Port *, int);
+/* void nseU_setnfield (lua_State *L, int idx,             [-0, +0, e]
+ *                      const char *field, lua_Number value)
+ *
+ * Sets the field for table at index idx to numerical value.
+ *  (t[field] = value).
+ */
+void nseU_setnfield (lua_State *L, int idx, const char *field, lua_Number value);
+
+/* void nseU_setbfield (lua_State *L, int idx,             [-0, +0, e]
+ *                      const char *field, int value)
+ *
+ * Sets the field for table at index idx to boolean value.
+ *  (t[field] = value).
+ */
+void nseU_setbfield (lua_State *L, int idx, const char *field, int value);
+
+/* void nseU_weaktable (lua_State *L, int narr, int nrec,  [-0, +1, e]
+ *                      const char *mode)
+ *
+ * Creates a table using lua_createtable with sizes narr and nrec. Creates
+ * a metatable with its __mode field set to mode.
+ */
+void nseU_weaktable (lua_State *L, int narr, int nrec, const char *mode);
+
+/* int nseU_success (lua_State *L)                         [-0, +1, -]
+ *
+ * Indicates successful return of the running function by pushing
+ * boolean true and returning 1. Use as a tail call:
+ *   return nseU_success(L);
+ */
+int nseU_success (lua_State *L);
+
+/* int nseU_safeerror (lua_State *L, const char *fmt, ...) [-0, +1, -]
+ *
+ * Indicates unsuccessful return of the running function by pushing
+ * boolean false and and a formatted error message. Use as a tail call:
+ *   return nseU_safeerror(L, "%s", "a generic error");
+ */
+int nseU_safeerror (lua_State *L, const char *fmt, ...);
+
+/* void nseU_typeerror (lua_State *L, const char *err,      [-0, +1, v]
+ *                      int idx)
+ *
+ * Raises a type error. Same as Lua 5.1.
+ */
+void nseU_typeerror (lua_State *L, const char *err, int idx);
+
+/* void *nseU_checkudata (lua_State *L, int idx,           [-0, +0, v]
+ *                        int upvalue, const char *name)
+ *
+ * Checks that value at index idx is a full userdata which a metatable
+ * equal to upvalue. name is the name of your object for error message
+ * purposes.
+ */
+void *nseU_checkudata (lua_State *L, int idx, int upvalue, const char *name);
+
+/* void nseU_checktarget (lua_State *L, int idx,           [-0, +0, v]
+ *                        const char **address,
+ *                        const char **targetname)
+ *
+ * Check for a valid target specification at index idx.  This function checks
+ * for a string at idx or a table containing the typical host table fields,
+ * 'ip' and 'targetname' in particular.
+ *
+ * The address and targetname string pointers are only valid if the target
+ * specification persists.
+ */
+void nseU_checktarget (lua_State *L, int idx, const char **address, const char **targetname);
+
+/* uint16_t nseU_checkport (lua_State *L, int idx,         [-0, +0, v]
+ *                          const char **protocol)
+ *
+ * Check for a valid port specification at index idx.
+ *
+ * The protocol string pointer is only valid if the port specification
+ * persists.
+ */
+uint16_t nseU_checkport (lua_State *L, int idx, const char **protocol);
+
+/* Target *nseU_gettarget (lua_State *L, int idx)          [-0, +0, v]
+ *
+ * This function checks the value at index for a valid host table. It locates
+ * the associated Target (C++) class object associated with the host and
+ * returns it. If the Target is not being scanned then an error will be raised.
+ */
+Target *nseU_gettarget (lua_State *L, int idx);
+
+/* Port *nseU_getport (lua_State *L, Target *target,       [-0, +0, v]
+ *                     Port *port, int idx)
+ *
+ * This function checks the value at index for a valid port table. It locates
+ * the associated Port (C++) class object associated with the host and
+ * returns it.
+ */
+Port *nseU_getport (lua_State *L, Target *target, Port *port, int idx);
 
 #endif
