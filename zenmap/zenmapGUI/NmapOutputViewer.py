dmiller-nmap/nmap

commit 7323b87f7d5f9a32043d40d569f8cee837f95748

Make NmapOutputViewer.go_to_host more efficient
Instead of loading the entire output into a new string with
gtk.TextBuffer.get_text, we use the gtk.TextIter.forward_search method.
This works because we don't need to use regular expressions to find a
static string.

git-svn-id: https://svn.nmap.org/nmap@32936 e0a8ed71-7df4-0310-8962-fdc924857419

zenmap/zenmapGUI/NmapOutputViewer.py
@@ -216,16 +216,14 @@ class NmapOutputViewer (gtk.VBox):
     def go_to_host(self, host):
         """Go to host line on nmap output result"""
         buff = self.text_view.get_buffer()
-        start_iter, end_iter = buff.get_bounds()
+        start_iter = buff.get_start_iter()
 
-        output = buff.get_text(start_iter, end_iter).split("\n")
-        re_host = re.compile(r'^Nmap scan report for %s\s*$' % re.escape(host))
-
-        for i in xrange(len(output)):
-            if re_host.match(output[i]):
-                self.text_view.scroll_to_iter(
-                        buff.get_iter_at_line(i), 0, True, 0, 0)
-                break
+        found = start_iter.forward_search(
+                "\nNmap scan report for %s\n" % host, gtk.TEXT_SEARCH_TEXT_ONLY
+                )[0]
+        if not found.forward_line():
+            return
+        self.text_view.scroll_to_iter(found, 0, True, 0, 0)
 
     def show_output_properties(self, widget):
         nmap_out_prop = NmapOutputProperties(self.text_view)

commit 215d5a44d618359fa702c6fb58aae40ec81ca8ac

Correct a bug in the code for NmapOutputViewer.go_to_host that was put in revision 32936.
The following steps caused the bug with the r32936 code:
    1. Start a scan (say Quick Scan for simplicity) for a certain target (say A)
    2. Wait for scan to complete
    3. Start a scan for a different target (say B).
    4. Wait for scan to complete
    5. Click on host A in the Hosts list on the left (while scan B is still visible on the right)
    6. BOOM!


git-svn-id: https://svn.nmap.org/nmap@32939 e0a8ed71-7df4-0310-8962-fdc924857419

zenmap/zenmapGUI/NmapOutputViewer.py
@@ -218,9 +218,13 @@ class NmapOutputViewer (gtk.VBox):
         buff = self.text_view.get_buffer()
         start_iter = buff.get_start_iter()
 
-        found = start_iter.forward_search(
+        found_tuple = start_iter.forward_search(
                 "\nNmap scan report for %s\n" % host, gtk.TEXT_SEARCH_TEXT_ONLY
-                )[0]
+                )
+        if found_tuple is None:
+                return
+
+        found = found_tuple[0]
         if not found.forward_line():
             return
         self.text_view.scroll_to_iter(found, 0, True, 0, 0)